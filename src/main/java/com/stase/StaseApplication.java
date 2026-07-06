d8dc0fb01e426a08081bdd6fe994d6b687adc557
BouyaSensei <buildario@outlook.fr>
2026-07-06 18:01:18 +0200

    Ajout de la structure de projet Maven avec les fichiers de configuration nécessaires, y compris le wrapper Maven, le fichier POM, et les classes de base pour l'application Spring Boot.

---
+++ src/main/java/com/stase/StaseApplication.java
@@ -0,0 +1,13 @@
+package com.stase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stase/StaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
