5058d469b01e8622065bfab8f458afd5976c0c3a
BouyaSensei <buildario@outlook.fr>
2026-07-07 03:50:39 +0200

    Ajout de l'entité user au projet

---
+++ src/main/java/com/stase/entities/User.java
@@ -0,0 +1,48 @@
+package com.stase.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable= false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+}
+
+
+
+
+
+
+    
+
