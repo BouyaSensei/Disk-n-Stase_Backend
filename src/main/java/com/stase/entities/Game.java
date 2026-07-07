09d97c22e268de329e3a38a0b3de6d4b0fc1b866
BouyaSensei <buildario@outlook.fr>
2026-07-07 04:02:18 +0200

    Ajout des getters et setters de l'entité Game au projet

--- src/main/java/com/stase/entities/Game.java
+++ src/main/java/com/stase/entities/Game.java
@@ -18,5 +18,48 @@ public class Game {
     private List<String> languages;
     private String coverImageUrl;
 
+    public Game() {
+    }
+    public Game(String title, Text description, String genre, Boolean isPhysical, List<String> languages, String coverImageUrl) {
+        this.title = title;
+        this.description = description;
+        this.genre = genre;
+        this.isPhysical = isPhysical;
+        this.languages = languages;
+        this.coverImageUrl = coverImageUrl;
 
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public Text getDescription() {
+        return description;
+    }
+    public void setDescription(Text description) {
+        this.description = description;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+
+    }
+    public Boolean getIsPhysical() {
+        return isPhysical;
+    }
+    public void setIsPhysical(Boolean isPhysical) {
+        this.isPhysical = isPhysical;
+    }
+    public List<String> getLanguages() {
+        return languages;
+    }
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
 }
