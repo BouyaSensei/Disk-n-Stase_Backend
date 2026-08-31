f1b94f248c9d8c562da63595192e05ef3c37897f
BouyaSensei <buildario@outlook.fr>
2026-08-31 13:06:15 +0200

    Ajout de l'URL de couverture dans GameRawgDto, mise à jour de la méthode convertToDto dans GameService et ajout de la méthode converToGame pour créer un objet Game à partir de GameRawgDto.

--- src/main/java/com/stase/services/GameService.java
+++ src/main/java/com/stase/services/GameService.java
@@ -2,10 +2,12 @@ package com.stase.services;
 
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stase.components.RawgProvider;
 import com.stase.dtos.game.rawg.GameRawgDto;
 import com.stase.dtos.game.rawg.ListGameRawgDto;
@@ -25,17 +27,44 @@ public class GameService {
     public GameRawgDto convertToDto(Game game) {
 
         return new GameRawgDto(game.getId(), game.getName(), game.getDescription(), game.getGenre(),
-                game.getIsPhysical());
+                game.getIsPhysical(), game.getCoverImageUrl());
 
     }
 
-    public List<ListGameRawgDto> GetAllGames() {
+    public Game converToGame(GameRawgDto dto) {
+        Game gameFresh = new Game();
+        gameFresh.setId(dto.id());
+        gameFresh.setName(dto.name());
+        gameFresh.setDescription(dto.description());
+
+        return gameFresh;
+
+        // return new Game(Game.setId(gameDto.id()));
+    }
+
+    public String listToJson(List<ListGameRawgDto> games) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(games);
+    }
+
+    public String getAllGames() {
+
         List<Game> localGames = gameRepository.findAll();
         List<ListGameRawgDto> remoteGames = rawgProvider.fetchAllGames();
 
         // corriger cet variable et trouver le moyen de transformer la list du repo en
         // dto
-        Set<Long> localIds = localGames.stream().map(GameRawgDto::id).collect(Collector.toSet());
+        if (localGames.isEmpty()) {
+            return listToJson(remoteGames);
+
+        } else {
+            Set<Long> localIds = localGames.stream().map((g) -> convertToDto(g)).map(GameRawgDto::id)
+                    .collect(Collectors.toSet());
+            List<ListGameRawgDto> newGames = remoteGames.stream().filter(game -> !localIds.contains(game.id()))
+                    .collect(Collectors.toList());
+            return listToJson(newGames);
+        }
+
     }
 
     public GameRawgDto getGame(Long id) {
