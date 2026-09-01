59725c8617f0fe42cb267e3026814c2c6077b062
BouyaSensei <buildario@outlook.fr>
2026-09-01 15:05:06 +0200

    Réorganisation de GameController : ajout de méthodes pour récupérer tous les jeux et les détails d'un jeu spécifique, mise à jour de la route API.

--- src/main/java/com/stase/controllers/GameController.java
+++ src/main/java/com/stase/controllers/GameController.java
@@ -1,10 +1,30 @@
 package com.stase.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stase.services.GameService;
+
 @RestController
-@RequestMapping("/api/games")
+@RequestMapping("/api")
 public class GameController {
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping("/games")
+    public String getAllGames() {
+        return gameService.getAllGames();
+    }
+
+    @GetMapping("/games/{id}")
+    public String getGameDetail(@PathVariable Long id) {
+
+        return gameService.getGame(id);
+    }
 
 }
